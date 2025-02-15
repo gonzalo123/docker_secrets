dbe6cc240bd04282e4d1dcd281e7bb59dc5ad471
Gonzalo Ayuso <gonzalo123@gmail.com>
2025-02-15 15:13:53 +0100

    wip

--- bin/containers_with_secret.sh
+++ bin/containers_with_secret.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m'
 
 if [ -z "$1" ]; then
-  echo "${RED}[ERROR]${NC} You must provide the secret name as a parameter."
+  echo "[ERROR] You must provide the secret name as a parameter."
   echo "Usage: $0 <SECRET_NAME>"
   exit 1
 fi
 
 SECRET_NAME="$1"
 
-echo "${GREEN}[INFO]${NC} Looking for services using secret: '$SECRET_NAME'..."
+echo "[INFO] Looking for services using secret: '$SECRET_NAME'..."
 SERVICES=$(docker service ls --format "{{.Name}}" | while read -r service; do
   if docker service inspect "$service" | jq -e ".[].Spec.TaskTemplate.ContainerSpec.Secrets | map(select(.SecretName == \"$SECRET_NAME\")) | length > 0" >/dev/null; then
     echo "$service"
@@ -20,11 +17,11 @@ SERVICES=$(docker service ls --format "{{.Name}}" | while read -r service; do
 done)
 
 if [ -z "$SERVICES" ]; then
-  echo "${GREEN}[INFO]${NC} No services using secret: '$SECRET_NAME'."
+  echo "[INFO] No services using secret: '$SECRET_NAME'."
   exit 0
 fi
 
-echo "${GREEN}[INFO]${NC} Containers using secret: '$SECRET_NAME':"
+echo "[INFO] Containers using secret: '$SECRET_NAME':"
 for service in $SERVICES; do
   docker ps --filter "name=${service}" --format "Container {{.ID}} ({{.Names}}) uses the service '$service'"
 done
--- bin/hot_secret_update.sh
+++ bin/hot_secret_update.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m'
-
 if [ "$#" -ne 2 ]; then
   echo "Usage: $0 <SECRET_NAME> <NEW_PASSWORD>"
   exit 1
@@ -15,37 +11,37 @@ NEW_PASSWORD=$2
 TEMP_SECRET_NAME="$SECRET_NAME.tmp"
 
 if ! docker secret ls --format "{{.Name}}" | grep -qw "$SECRET_NAME"; then
-  echo "${RED}[ERROR]${NC} Secret '$SECRET_NAME' does not exist."
-  echo "${GREEN}[INFO]${NC} Available secrets in the cluster:"
+  echo "[ERROR] Secret '$SECRET_NAME' does not exist."
+  echo "[INFO] Available secrets in the cluster:"
   docker secret ls --format "table {{.ID}}\t{{.Name}}\t{{.CreatedAt}}"
   exit 1
 fi
 
-echo "${GREEN}[INFO]${NC} Creating new temporal secret '$TEMP_SECRET_NAME'..."
+echo "[INFO] Creating new temporal secret '$TEMP_SECRET_NAME'..."
 echo "$NEW_PASSWORD" | docker secret create "$TEMP_SECRET_NAME" -
 
-echo "${GREEN}[INFO]${NC} Looking for services using secret: '$SECRET_NAME'..."
+echo "[INFO] Looking for services using secret: '$SECRET_NAME'..."
 SERVICES=$(docker service ls --format "{{.Name}}" | while read -r service; do
   if docker service inspect "$service" | jq -e ".[].Spec.TaskTemplate.ContainerSpec.Secrets | map(select(.SecretName == \"$SECRET_NAME\")) | length > 0" >/dev/null; then
     echo "$service"
   fi
 done)
 
-echo "${GREEN}[INFO]${NC} Updating services to use the new secret and remove the old one..."
+echo "[INFO] Updating services to use the new secret and remove the old one..."
 for service in $SERVICES; do
   docker service update --secret-add "$TEMP_SECRET_NAME" --secret-rm "$SECRET_NAME" "$service"
 done
 
-echo "${GREEN}[INFO]${NC} Removing the old secret..."
+echo "[INFO] Removing the old secret..."
 docker secret rm "$SECRET_NAME"
 echo "$NEW_PASSWORD" | docker secret create "$SECRET_NAME" -
 
-echo "${GREEN}[INFO]${NC} Renaming the new secret..."
+echo "[INFO] Renaming the new secret..."
 for service in $SERVICES; do
   docker service update --secret-add "$SECRET_NAME" --secret-rm "$TEMP_SECRET_NAME" "$service"
 done
 
-echo "${GREEN}[INFO]${NC} Removing the temporary secret..."
+echo "[INFO] Removing the temporary secret..."
 docker secret rm "$TEMP_SECRET_NAME"
 
-echo "${GREEN}[INFO]${NC} Secret '$SECRET_NAME' updated successfully."
+echo "[INFO] Secret '$SECRET_NAME' updated successfully."
